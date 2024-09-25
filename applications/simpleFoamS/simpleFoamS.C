c9104ad26328989381d1d68ab9c4787e64a7ec74
Saeede <saeedehpr1994@gmail.com>
2024-09-24 23:10:54 -0400

    Updated application.zip

--- applications/simpleFoamS/simpleFoamS.C
+++ applications/simpleFoamS/simpleFoamS.C
@@ -102,7 +102,8 @@ int main(int argc, char *argv[])
 	    // Calculate the continuity residual
 		
         turbulence->correct();
-		continuityResidual = mag(fvc::div(phi, U)); // Update the continuity residual
+		continuityResidual = mag(fvc::div(U)); // Update the continuity residual
+		U_phi = mag(fvc::div(phi, U)); 
         runTime.write();
         Info<< "ExecutionTime = " << runTime.elapsedCpuTime() << " s"
             << "  ClockTime = " << runTime.elapsedClockTime() << " s"
